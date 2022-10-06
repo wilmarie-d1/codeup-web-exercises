77688efe0c066fe9c1e7530274622b142a7da1f2
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-10-06 12:37:40 -0500

    commit update

--- JS/Intro-JS.js
+++ JS/Intro-JS.js
@@ -1,2 +1,14 @@
+
+"use strict";
+
 let greeting = "Hello from an external JS file!";
 console.log(greeting);
+
+alert("You've been hacked!");
+prompt("What is your name?");
+let userName = prompt("What is your name?");
+    console.log(userName);
+let isFull = confirm("Is it Full?");
+console.log(isFull);
+
+alert("Hello" + userName + "welcome to my other website");
---
+++ JS/function-ics.js
@@ -0,0 +1,29 @@
+"use strict";// ================ !! Mini-exercises !!
+
+// Write a function, returnFive, that returns the number five. No inputs should be defined.
+
+function returnFive (5) {
+    x = 5;
+    return = x;
+
+    returnFive();
+}
+// Write a function, isFive, that takes in an input and returns the boolean value true if the passed argument is the number 5 or the string "5". Return false otherwise.
+
+// how many inputs should the function have?
+// what are the data types for the inputs?
+// what is the data type of the output/return value?
+
+
+// Write a function, isShortWord, that takes in a string and returns the boolean value true if the passed argument is shorter than 5 characters. Return false otherwise.
+
+// how many inputs?
+// what data type for inputs?
+// what is the return type?
+
+
+// Write a function, isSameLength, that takes in two string inputs and returns the boolean value true if the passed arguments are the same length. Return false otherwise.
+
+// How many inputs?
+// What type of input?
+// What type of output?
