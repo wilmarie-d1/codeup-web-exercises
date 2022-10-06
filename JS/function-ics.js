e5e804aa01fee255b3002867b6e900812425f486
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-10-06 14:38:05 -0500

    my function

--- JS/function-ics.js
+++ JS/function-ics.js
@@ -2,28 +2,46 @@
 
 // Write a function, returnFive, that returns the number five. No inputs should be defined.
 
-function returnFive (5) {
-    x = 5;
-    return = x;
-
-    returnFive();
+function returnFive() {
+    return 5;
 }
+console.log(returnFive());
 // Write a function, isFive, that takes in an input and returns the boolean value true if the passed argument is the number 5 or the string "5". Return false otherwise.
 
-// how many inputs should the function have?
-// what are the data types for the inputs?
-// what is the data type of the output/return value?
+function isFive (number) {
+    return number === 5 || number ==="5";
+
+}
+
+console.log(isFive(5));
+console.log(isFive("5.5"));
+console.log(isFive("5"));
+
+// how many inputs should the function have? 1
+// what are the data types for the inputs? number or strong
+// what is the data type of the output/return value? boolean
 
 
 // Write a function, isShortWord, that takes in a string and returns the boolean value true if the passed argument is shorter than 5 characters. Return false otherwise.
+//const isShortWord = (string) => string.length < 5
+function isShortWord(word)  {
+    return word.length < 5;
+}
+console.log(isShortWord('four'));
 
-// how many inputs?
-// what data type for inputs?
-// what is the return type?
+
+// how many inputs? 1
+// what data type for inputs? string
+// what is the return type? boolean
 
 
 // Write a function, isSameLength, that takes in two string inputs and returns the boolean value true if the passed arguments are the same length. Return false otherwise.
+function isSameLength (word, road) {
+    return word.length === road.length;
 
-// How many inputs?
-// What type of input?
-// What type of output?
+}
+console.log(isSameLength("wash","wash"));
+
+// How many inputs? 2
+// What type of input? string, string
+// What type of output? boolean
