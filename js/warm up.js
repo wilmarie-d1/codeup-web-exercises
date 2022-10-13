d5bdfba8f6800ed615ea5ee0ed85ec5ef7cd3dd9
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-10-13 09:40:45 -0500

    warm up 10/13

--- js/warm up.js
+++ js/warm up.js
@@ -11,3 +11,38 @@ function warmUp (inputNum) {
     }
 }
 console.log(warmUp(6) );
+
+
+
+
+
+// Write a function named "inBetween" that accepts three inputs: min, max, num, and returns a boolean determining whether or not the "num" parameter is in between the min and max numbers.
+//
+//     EX:
+// inBetween(1, 10, 5) returns true
+// inBetween(25, 26, 25) returns false
+// inBetween(0, 1, 0.5) returns true
+
+"use strict"
+
+function inBetween (min, max, num) {
+    // let boolean;
+    if (min < num && num < max) {
+        //  boolean = true;
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(inBetween(5,10,7));
+console.log(inBetween(45,60,37));
+console.log(inBetween(45,100,76));
+
+
+
+
+
+
+
+
+
