a7cf2fa8b3f835f59c0bedf5809095e1d680d7b4
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-10-21 11:35:44 -0500

    forms exercise

--- js/arrays-lec.js
+++ js/arrays-lec.js
@@ -1,29 +1,26 @@
 // "use strict"
 
-// [1,2,3,4,5,6]
-// [""]
+[1,2,3,4,5,6]
+[""]
 let names = ["john", "jim", "will", "james", "paul"];
-//
-// function hello(){
-//
-// }
-// let arr = [true, 1, "hello", hello];
-// console.log(arr);
-// console.log(arr.length);
-// let str = arr.toString()
-// let str1 = arr.join();
-// console.log(str);
-// console.log(typeof str);
-// console.log(names[2]);
-// console.log(names[0])
-// console.log(names.length);
-// console.log(names.lastIndexOf("will"));
+
+let arr = [true, 1, "hello", hello];
+console.log(arr);
+console.log(arr.length);
+let str = arr.toString()
+let str1 = arr.join();
+console.log(str);
+console.log(typeof str);
+console.log(names[2]);
+console.log(names[0])
+console.log(names.length);
+console.log(names.lastIndexOf("will"));
 let newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// console.log(newArr);
-//
-// for (let i = 0; i < newArr.length; i++){
-//     console.log(names[i]);
-// }
+console.log(newArr);
+
+for (let i = 0; i < newArr.length; i++){
+    console.log(names[i]);
+}
 let lastIndex = names.length -1;
 
 for (let i = lastIndex; i >= 0; i--){
