e7e73647f7a254ce67ffa64b644884d8702995db
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-11-16 12:40:57 -0600

    mouse event exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -35,7 +35,33 @@ $(function(){
 
         // let contents = $('h1').html();
         // alert(contents);
+        //
+        // Use the file jquery_exercises.html for these exercises. Commit your changes to GitHub.
+        //
+        //     Remove your custom jQuery code from previous exercises.
+        //
+        //     Add jQuery code that will change the background color of an h1 element when clicked.
+        //
+        //     Make all paragraphs have a font size of 18px when they are double clicked.
+        //
+        //     Set all li text color to red when the mouse is hovering; reset to black when it is not.
 
+                $('h1').on('click', function () {
+                    $('h1').css('background-color', 'pink');
+                })
+
+                $('p').on('dblclick', function () {
+                    $('p').css('font-size', '18px');
+                })
+
+                 $('li').hover(
+            function() {
+                $('li').css('color', 'red');
+            },
+            function() {
+                $('li').css('color', 'black');
+            }
+        );
 
 
 
