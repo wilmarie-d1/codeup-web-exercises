7bf647a00e6cffca2fd580d1c188b8322e742e40
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-11-16 11:21:13 -0600

    exercise completion

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,12 +1,43 @@
 "use strict";
 
 // (function() {
-
-    $(function() {
-        alert('The DOM has finished loading!');
-    })
+//
+//     $(function() {
+//         alert('The DOM has finished loading!');
+//     })
 
 // ${'h2'}.html("I changed it");
 
 
 // });
+$(function(){
+    $(document).ready(function() {
+
+        // let contents = $('#h1-id').html()
+        // alert(contents);
+    //
+    //     let insideStuff = $('#jquery').html();
+    //     alert(insideStuff);
+    //
+    //     $('p').css('background-color', 'pink')
+    //     $('h1').css('background-color', "yellow")
+    //
+    // })
+        //Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.
+        //     $('li').css('font-size', "20px");
+            // $('.codeup').css({"border-color": "red",
+            // "border-width":"1px",
+            // "border-style":"solid"});
+
+        // $('li').css('border', 'solid 1px red');
+//Craft selectors that highlight all the h1, p, and li elements.
+//         $('h1, p, li').css('background-color', 'pink');
+
+        // let contents = $('h1').html();
+        // alert(contents);
+
+
+
+
+    })
+});
