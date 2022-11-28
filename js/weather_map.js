ec6888fdc808bd5bed1cf7ebf5476918c2832776
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-11-28 15:51:46 -0600

    adding 5 day forecast to map

--- js/weather_map.js
+++ js/weather_map.js
@@ -51,6 +51,48 @@ let ajaxCall = (arr) => {
         console.log(forecasts)
     })
 }
+    // let city = data.city.name
+    // let country = data.city.country
+
+let append = (data) => {
+    let html = ``
+    for  (let i = 0; i < data.length; i += 8){
+        console.log(data[i]);
+        const {dt_txt,
+            main: {humidity, temp, temp_max, temp_min},
+            weather: [{description, icon}], wind: {speed}} = data[i]
+
+        html += `
+                    <div class="card" style="width: 20%;">
+               <h6 class="card-header text-center text-dark opacity-75 mb-2" style="font-size: small">
+                ${dt_txt.substring(5,7)}.${dt_txt.substring(8,10)}.${dt_txt.substring(0,4)}
+               </h6>
+               <img src="http://openweathermap.org/img/wn/${icon}.png alt="img">
+              <div class="card-body pt-0">
+              <h4 class="card-title text-center">${temp.toFixed(1)}ºF</h4>
+                <div class="d-flex justify-content-around" >
+                <p class="card-text mb-0 text-warning pe-1" style="font-size: small;">
+                H:${temp_max.toFixed(1)}ºF
+                </p>
+
+                <p class="card-text mb-0 text-warning" style="font-size: small">
+                L:${temp_min.toFixed(1)}ºF
+                </p>
+                </div>
+
+              </div>
+              <ul class="list-group list-group-flush text-center">
+
+                <li class="humidity list-group-item text-dark" style="font-size: small">Humidity: ${humidity}%</li>
+                <li class="speed list-group-item text-dark" style="font-size: small">Wind: ${speed} mph</li>
+              </ul>
+              <div class="card-body p-0 ">
+              <p class="list-group-item bg-secondary text-light text-center p-0 m-0">${description.toUpperCase()}</p>
+              </div>
+            </div>
+`}
+    return html
+}
 // const successCallback = (position) => {
 //     console.log(position);
 // };
