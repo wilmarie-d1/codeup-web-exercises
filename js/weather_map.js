e265472cc8efaf0d9192ec227a10bcf2e8ddb131
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-11-29 15:09:39 -0600

    fixing the 5 day forecast cards, added bg image, added init function for geolocation

--- js/weather_map.js
+++ js/weather_map.js
@@ -49,10 +49,14 @@ let ajaxCall = (arr) => {
         let forecastHTML = append(forecasts)
         $('#weather').html(forecastHTML)
         console.log(forecasts)
+
+        let city = data.city.name
+        let country = data.city.country
+        $('#city-name').html(`${city}, ${country}`);
     })
+
 }
-    // let city = data.city.name
-    // let country = data.city.country
+
 
 let append = (data) => {
     let html = ``
@@ -63,19 +67,23 @@ let append = (data) => {
             weather: [{description, icon}], wind: {speed}} = data[i]
 
         html += `
-                    <div class="card" style="width: 20%;">
-               <h6 class="card-header text-center text-dark opacity-75 mb-2" style="font-size: small">
+                                  
+<div class="container text-center">
+  <div class="row">
+                    <div class="card col">
+               <h6 class="card-header text-center" style="font-size: small">
                 ${dt_txt.substring(5,7)}.${dt_txt.substring(8,10)}.${dt_txt.substring(0,4)}
                </h6>
-               <img src="http://openweathermap.org/img/wn/${icon}.png alt="img">
-              <div class="card-body pt-0">
+  <img src='http://openweathermap.org/img/w/${icon}.png' class="img-thumbnail d-block border-0" style='width: 100px; height: 100px;' alt="..."              <div class="card-body pt-0">
+                           <div class="card-body pt-0">
+
               <h4 class="card-title text-center">${temp.toFixed(1)}ºF</h4>
                 <div class="d-flex justify-content-around" >
-                <p class="card-text mb-0 text-warning pe-1" style="font-size: small;">
+                <p class="card-text  text-warning pe-1" style="font-size: small;">
                 H:${temp_max.toFixed(1)}ºF
                 </p>
 
-                <p class="card-text mb-0 text-warning" style="font-size: small">
+                <p class="card-text " style="font-size: small">
                 L:${temp_min.toFixed(1)}ºF
                 </p>
                 </div>
@@ -83,22 +91,43 @@ let append = (data) => {
               </div>
               <ul class="list-group list-group-flush text-center">
 
-                <li class="humidity list-group-item text-dark" style="font-size: small">Humidity: ${humidity}%</li>
-                <li class="speed list-group-item text-dark" style="font-size: small">Wind: ${speed} mph</li>
+                <li class="humidity list-group-item" style="font-size: small">Humidity: ${humidity}%</li>
+                <li class="speed list-group-item" style="font-size: small">Wind: ${speed} mph</li>
               </ul>
               <div class="card-body p-0 ">
-              <p class="list-group-item bg-secondary text-light text-center p-0 m-0">${description.toUpperCase()}</p>
+              <p class="list-group-item bg-secondary text-light text-center ">${description.toUpperCase()}</p>
               </div>
             </div>
+            </div>
+            </div>
+            
 `}
     return html
 }
-// const successCallback = (position) => {
-//     console.log(position);
-// };
-//
-// const errorCallback = (error) => {
-//     console.log(error);
-// };
-//
-// navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+//REV-GEO LOCATION DISPLAY FUNCTION
+let revGeo = (lng, lat) => {
+    reverseGeocode({lng, lat}, MAPBOX_TOKEN).then(function(results){
+        console.log(results)
+        $('#city').text(`${results}`)
+
+    })
+}
+
+(function () {
+    const successCallback = (position) => {
+        console.log(position);
+        let lat = (position.coords.latitude)
+        let lng = (position.coords.longitude)
+        revGeo(lng, lat)
+    };
+
+    const errorCallback = (error) => {
+        console.log(error);
+    };
+
+    navigator.geolocation.watchPosition(successCallback, errorCallback);
+
+
+
+})();
+
