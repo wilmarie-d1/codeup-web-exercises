f7d5085e0924fd5d9c39fd7e1387e392face3622
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-11-30 15:23:18 -0600

    got geolocation to work

--- js/weather_map.js
+++ js/weather_map.js
@@ -7,7 +7,6 @@ mapboxgl.accessToken = MAPBOX_TOKEN;
 console.log("token" + accessToken)
 
 
-
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/navigation-night-v1',
@@ -19,6 +18,9 @@ let map = new mapboxgl.Map({
 
 map.addControl(new mapboxgl.NavigationControl());
 
+// window.onload() {
+
+
 $('#search-btn').click(() => {
     const search = $('#search-input').val()
     console.log(search)
@@ -28,13 +30,20 @@ $('#search-btn').click(() => {
         map.setZoom(11)
 
         let marker = new mapboxgl.Marker()
-            .setLngLat([location[0],location[1]])
+            .setLngLat([location[0], location[1]])
             .addTo(map)
 
         ajaxCall(location)
         revGeo(location[0], location[1])
-    })
 
+    })
+    marker.on('dragend', function onDragEnd() {
+        const lngLat = marker.getLngLat();
+        let coordinates = lngLat.toArray()
+        console.log(lngLat.toArray())
+        getWeatherData(coordinates)
+        // console.log('Drag function:', 'lat:' + lngLat.lat, '/typeof: ' + typeof lngLat.lat, '/lng:' + lngLat.lng, '/typeof: ' + typeof lngLat.lng);
+    });
 })
 
 let ajaxCall = (arr) => {
@@ -43,7 +52,7 @@ let ajaxCall = (arr) => {
         lat: arr[1],
         lon: arr[0],
         units: "imperial"
-    }).done(function(data) {
+    }).done(function (data) {
 
         let forecasts = data.list
         let forecastHTML = append(forecasts)
@@ -57,73 +66,73 @@ let ajaxCall = (arr) => {
 
 }
 
-//
-// let append = (data) => {
-//     let html = ``
-//     for  (let i = 0; i < data.length; i += 8){
-//         console.log(data[i]);
-//         const {dt_txt,
-//             main: {humidity, temp, temp_max, temp_min},
-//             weather: [{description, icon}], wind: {speed}} = data[i]
-//
-//         html += `
-//        <div class="container text-center">
-//   <div class="row">
-//                     <div class="card col">
-//                <h6 class="card-header text-center" style="font-size: small">
-//                 ${dt_txt.substring(5,7)}.${dt_txt.substring(8,10)}.${dt_txt.substring(0,4)}
-//                </h6>
-//   <img src='http://openweathermap.org/img/w/${icon}.png' class="img-thumbnail d-block border-0" style='width: 100px; height: 100px;' alt="...">
-//     <div class="card-body pt-0">
-//                            <div class="card-body pt-0">
-//               <h4 class="card-title text-center">${temp.toFixed(1)}ºF</h4>
-//                 <div class="d-flex justify-content-around" >
-//                 <p class="card-text  text-warning pe-1" style="font-size: small;">
-//                 H:${temp_max.toFixed(1)}ºF
-//                 </p>
-//                 <p class="card-text " style="font-size: small">
-//                 L:${temp_min.toFixed(1)}ºF
-//                 </p>
-//                 </div>
-//               </div>
-//               <ul class="list-group text-center">
-//                 <li class="humidity list-group-item" style="font-size: small">Humidity: ${humidity}%</li>
-//                 <li class="speed list-group-item" style="font-size: small">Wind: ${speed} mph</li>
-//               </ul>
-//               <div class="card-body p-0 ">
-//               <p class="list-group-item  ">${description.toUpperCase()}</p>
-//               </div>
-//             </div>
-//             </div>
-//             </div>
-//
-// `}
-//     return html
-// }
+
+let append = (data) => {
+    let html = ``
+    for (let i = 0; i < data.length; i += 8) {
+        console.log(data[i]);
+        const {
+            dt_txt,
+            main: {humidity, temp, temp_max, temp_min},
+            weather: [{description, icon}], wind: {speed}
+        } = data[i]
+
+        html += `
+                    <div class="card" style=width:18rem;">
+               <h6 class="card-header text-center mb-2" style="font-size: small">
+                ${dt_txt.substring(5, 7)}.${dt_txt.substring(8, 10)}.${dt_txt.substring(0, 4)}
+               </h6>
+  <img src='http://openweathermap.org/img/w/${icon}.png' class="img-thumbnail d-block border-0" style='width: 100px; height: 100px; margin-left: 0' alt="...">
+                 <div class="card-body pt-0">
+                           <div class="card-body pt-0">
+              <h4 class="card-title text-center">${temp.toFixed(1)}ºF</h4>
+                <div class="d-flex justify-content-around" >
+                <p class="card-text  text-warning pe-1" style="font-size: small;">
+                H:${temp_max.toFixed(1)}ºF
+                </p>
+                <p class="card-text " style="font-size: small">
+                L:${temp_min.toFixed(1)}ºF
+                </p>
+                </div>
+              </div>
+                <ul class="list-group list-group-flush text-center">
+                <li class="humidity list-group-item" style="font-size: small">Humidity: ${humidity}%</li>
+                <li class="speed list-group-item" style="font-size: small">Wind: ${speed} mph</li>
+              </ul>
+              <div class="card-body p-0 ">
+              <p class="list-group-item  ">${description.toUpperCase()}</p>
+              </div>
+            </div>
+            </div>
+            </div>
+`}
+    return html
+}
 //REV-GEO LOCATION DISPLAY FUNCTION
 let revGeo = (lng, lat) => {
-    reverseGeocode({lng, lat}, MAPBOX_TOKEN).then(function(results){
+    reverseGeocode({lng, lat}, MAPBOX_TOKEN).then(function (results) {
         console.log(results)
         $('#city').text(`${results}`)
 
     })
 }
 
-(function () {
+function geoLocation() {
     const successCallback = (position) => {
         console.log(position);
         let lat = (position.coords.latitude)
         let lng = (position.coords.longitude)
-        revGeo(lng, lat)
+        let currentLocation = [lng, lat];
+
+        console.log(ajaxCall(currentLocation));
     };
 
     const errorCallback = (error) => {
         console.log(error);
     };
 
-    navigator.geolocation.watchPosition(successCallback, errorCallback);
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+};
 
-
-
-})();
+console.log(geoLocation());
 
